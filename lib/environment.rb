79dcd34804dcf9721cc3614ee3331604e8f0c682
Chandra Lama <iamme24cl@gmail.com>
2020-10-16 01:02:59 -0700

    updated environment file and gemspec file

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,12 @@
-require "top_CA_wineries/version"
+require_relative "./top_CA_wineries/version"
+require_relative "./top_CA_wineries/scraper"
+require_relative "./top_CA_wineries/cli"
+require_relative "./top_CA_wineries/region"
+require_relative "./top_CA_wineries/winery"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 
 module TopCAWineries
   class Error < StandardError; end
---
+++ lib/top_CA_wineries/cli.rb
@@ -0,0 +1,7 @@
+class TopCAWineries::CLI
+    def greet
+        puts "Welcome to TopCAWineries! Let's learn about some of the best California wineries."
+        puts "Pick a Region from below to see all the best wineries there. Type in the correspoding number:"
+        binding.pry
+    end
+end
