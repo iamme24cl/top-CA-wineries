126ea31f4311cc8fefe35f7ae7c8432d974a5fe1
Chandra Lama <iamme24cl@gmail.com>
2020-10-16 19:20:54 -0700

    updated cli file and added methods with fake data for program function verification

--- lib/top_CA_wineries/cli.rb
+++ lib/top_CA_wineries/cli.rb
@@ -1,7 +1,52 @@
 class TopCAWineries::CLI
-    def greet
-        puts "Welcome to TopCAWineries! Let's learn about some of the best California wineries."
-        puts "Pick a Region from below to see all the best wineries there. Type in the correspoding number:"
-        binding.pry
+    def call
+        greet_and_display
+        display_wineries
+        display_winery_info
     end
+        
+
+
+    def greet_and_display
+        puts <<-DOC.gsub /^\s*/, ""
+            Welcome to TopCAWineries! Let's learn about some of the best California wineries.
+            Pick a Region from below to see all the best wineries there.
+            1. Mendocino County
+            2. Sonoma County
+            3. Napa Valley
+            4. East Bay and Livermore
+            5. Monterey and the Santa Cruz Mountains
+            6. Paso Robles and San Luis Obispo County
+        DOC
+    end
+
+    def display_wineries
+        puts <<-DOC.gsub /^\s*/, ""
+            Enter the number of the region you would like to see wineries in:
+        DOC
+       
+        input = gets.strip.to_i
+    
+        if input == 1
+            puts <<-DOC.gsub /^\s*/, ""
+                1. Campovida
+                2. Drew
+                3. Foursight
+                4. Longmeadow Ranch
+                5. Pennyroyal Farm
+                6. Phillips Farm
+                7. Roederer
+                8. Terra Savia
+            DOC
+        end
+    end
+
+    def display_winery_info
+        
+    end
+        
 end
+        
+
+    
+
---
+++ notes.md
@@ -0,0 +1,23 @@
+Top CA Wineries:
+
+1. Displays a greeting and a list of Wine regions and asks user to select from the wine region
+2. Get user input
+3. Display the wineries in the region requested by the user
+4. Ask user which winery they would like more information on
+5. Get user input
+6. Display the information on the winery that the user requested
+5. Ask user if they would like to go back to the previous winery menu or the main region menu or exit
+
+# Scrape data at:
+1. url: "https://thepress.sfchronicle.com/article/52-california-wineries-2019/"
+2. reviews_url: "https://thepress.sfchronicle.com/review/"
+3. Region names: doc.css("div.entry-content h1")
+4. Winery names: doc.css("div.entry-content p a b")
+5. Winery information: doc.css("div.entry-content p") 
+    - Wine info array
+    a. Mendocino: [11..18]
+    b. Sonoma: [21..32]
+    c. Napa Valley: [35..45]
+    d. East Bay: [48..53]
+    e. Monterey: [56..60]
+    f. Paso Robles: [64..73]
