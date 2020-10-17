95f5c01168ab271cacd995ec391894c98b1169aa
Chandra Lama <iamme24cl@gmail.com>
2020-10-17 01:48:27 -0700

    Scraper class is done i think..created all class methods

--- lib/top_CA_wineries/scraper.rb
+++ lib/top_CA_wineries/scraper.rb
@@ -1,18 +1,51 @@
 class TopCAWineries::Scraper
-    attr_accessor :path
+    attr_accessor 
+    @@doc = []
+    @@region_array = []
+    @@winery_array = []
+    @@winery_info_array = []
 
     def initialize
-        
     end
-        
         
     def self.scrape
         site = "https://thepress.sfchronicle.com/article/52-california-wineries-2019/" 
-        doc = Nokogiri::HTML(open(site))
+        @@doc = Nokogiri::HTML(open(site))
     end
-   
+    
+    def self.region_array
+        array = @@doc.css("div.entry-content h1").map do |region|
+            region.text
+        end
+        @@region_array = array
+    end
 
+    def self.winery_array
+        array = @@doc.css("div.entry-content p a b").map do |winery|
+            winery.text
+        end
+        @@winery_array = array
+    end
+
+    def self.winery_info_array
+        array = @@doc.css("div.entry-content p").map do |info|
+            info.text
+        end
+        # remove \n from info
+        modified_array = array.map do |info|
+            info.gsub("\n", ": ")
+        end
+        @@winery_info_array = modified_array
+    end
 end
 
 
 
+        
+        
+   
+
+
+
+
+# s = TopCAWineries::Scraper.new
