21f5328171895274ac5493f005833430517d0fec
Chandra Lama <iamme24cl@gmail.com>
2020-10-27 13:38:01 -0700

    refactored display_winreies_info to take in an argument of region name and find the winery descriptions for region matching the region name.

--- lib/top_CA_wineries/cli.rb
+++ lib/top_CA_wineries/cli.rb
@@ -43,21 +43,14 @@ class TopCAWineries::CLI
         
         case region_input
         when 1
-            puts "Here are some of the best wineries in Mendocino County"
-            puts "\n"
-            display_mendocino = TopCAWineries::WineRegion.all[0].wineries
-            display_mendocino.each.with_index(1) do |winery, i|
-                puts "#{i}. #{winery.name.colorize(:magenta)}"
-            end
-            puts "\n"
-            puts "Want some details about these wineries?? Type 'yes' to continue or 'exit' to leave the program:".colorize(:red)
-
+            display_wineries("Mendocino County")
+            # Display description of wineries
             details_input = nil
             while details_input != 'yes'
                 details_input = gets.strip.downcase
 
                 if details_input == 'yes'
-                    display_region("Mendocino County")
+                    display_region_wineries_info("Mendocino County")
                 elsif details_input == 'exit'
                     bye
                     exit
@@ -67,20 +60,13 @@ class TopCAWineries::CLI
             end
             
         when 2
-            puts "Here are some of the best wineries in Sonoma County"
-            puts "\n"
-            display_sonoma = TopCAWineries::WineRegion.all[1].wineries
-            display_sonoma.each.with_index(1) do |winery, i|
-                puts "#{i}. #{winery.name.colorize(:green)}"
-            end
-            puts "\n"
-            puts "Want some details about these wineries?? Type 'yes' to continue or 'exit' to leave the program:".colorize(:red)
-
+            display_wineries("Sonoma County")
+            # Display description of wineries
             while details_input != 'yes'
                 details_input = gets.strip.downcase
 
                 if details_input == 'yes'
-                    display_region("Sonoma County")
+                    display_region_wineries_info("Sonoma County")
                 elsif details_input == 'exit'
                     bye
                     exit
@@ -90,20 +76,13 @@ class TopCAWineries::CLI
             end
          
         when 3
-            puts "Here are some of the best wineries in Napa Valley"
-            puts "\n"
-            display_napa = TopCAWineries::WineRegion.all[2].wineries
-            display_napa.each.with_index(1) do |winery, i|
-                puts "#{i}. #{winery.name.colorize(:magenta)}"
-            end
-            puts "\n"
-            puts "Want some details about these wineries?? Type 'yes' to continue or 'exit' to leave the program:".colorize(:red)
-
+            display_wineries("Napa Valley")
+            # Display description of wineries
             while details_input != 'yes'
                 details_input = gets.strip.downcase
 
                 if details_input == 'yes'
-                    display_region("Napa Valley")
+                    display_region_wineries_info("Napa Valley")
                 elsif details_input == 'exit'
                     bye
                     exit
@@ -113,20 +92,13 @@ class TopCAWineries::CLI
             end
 
         when 4
-            puts "Here are some of the best wineries in East Bay and Livermore"
-            puts "\n"
-            display_east_bay = TopCAWineries::WineRegion.all[3].wineries
-            display_east_bay.each.with_index(1) do |winery, i|
-                puts "#{i}. #{winery.name.colorize(:green)}"
-            end
-            puts "\n"
-            puts "Want some details about these wineries?? Type 'yes' to continue or 'exit' to leave the program:".colorize(:red)
-
+            display_wineries("East Bay")
+            # Display description of wineries
             while details_input != 'yes'
                 details_input = gets.strip.downcase
 
                 if details_input == 'yes'
-                    display_region("East Bay")
+                    display_region_wineries_info("East Bay")
                 elsif details_input == 'exit'
                     bye
                     exit
@@ -136,20 +108,13 @@ class TopCAWineries::CLI
             end
 
         when 5
-            puts "Here are some of the best wineries in Monterey and the Santa Cruz Mountains"
-            puts "\n"
-            display_monterey = TopCAWineries::WineRegion.all[4].wineries
-            display_monterey.each.with_index(1) do |winery, i|
-                puts "#{i}. #{winery.name.colorize(:magenta)}"
-            end
-            puts "\n"
-            puts "Want some details about these wineries?? Type 'yes' to continue or 'exit' to leave the program:".colorize(:red)
-
-           while details_input != 'yes'
+            display_wineries("Monterey County and Santa Cruz Mountains")
+            # Display description of wineries
+            while details_input != 'yes'
                 details_input = gets.strip.downcase
 
                 if details_input == 'yes'
-                    display_region("Monterey County and Santa Cruz Mountains")
+                    display_region_wineries_info("Monterey County and Santa Cruz Mountains")
                 elsif details_input == 'exit'
                     bye
                     exit
@@ -159,20 +124,13 @@ class TopCAWineries::CLI
             end
 
         when 6
-            puts "Here are some of the best wineries in Paso Robles and San Luis Obispo County"
-            puts "\n"
-            display_paso_robles = TopCAWineries::WineRegion.all[5].wineries
-            display_paso_robles.each.with_index(1) do |winery, i|
-                puts "#{i}. #{winery.name.colorize(:green)}"
-            end
-            puts "\n"
-            puts "Want some details about these wineries?? Type 'yes' to continue or 'exit' to leave the program:".colorize(:red)
-
+            display_wineries("Paso Robles and and San Luis Obispo County")
+            # Display description of wineries
             while details_input != 'yes'
                 details_input = gets.strip.downcase
 
                 if details_input == 'yes'
-                    display_region("Paso Robles and and San Luis Obispo County")
+                    display_region_wineries_info("Paso Robles and and San Luis Obispo County")
                 elsif details_input == 'exit'
                     bye
                     exit
@@ -188,7 +146,20 @@ class TopCAWineries::CLI
     end
 
     # Display winery-info hepler methods
-    def display_region(name)
+    
+    def display_wineries(name)
+        puts "Here are some of the best wineries in #{name}"
+        puts "\n"
+
+        TopCAWineries::WineRegion.all.find {|region| region.name.downcase == name.downcase}.wineries.each.with_index(1) do |winery, i|
+            puts "#{i}. #{winery.name.colorize(:green)}"
+        end
+
+        puts "\n"
+        puts "Want some details about these wineries?? Type 'yes' to continue or 'exit' to leave the program:".colorize(:red)
+    end
+
+    def display_region_wineries_info(name)
         TopCAWineries::WineRegion.all.find {|region| region.name.downcase == name.downcase}.wineries.each.with_index(1) do |winery, i|
             puts "#{i}. #{winery.description.colorize(:cyan)}"
             puts "\n"
